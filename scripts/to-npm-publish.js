12e0d4fde815271b2f79ac3b093fc5e5232837cb
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-11-21 16:07:01 +0400

    to-npm-publish script imporovements

--- scripts/to-npm-publish.js
+++ scripts/to-npm-publish.js
@@ -1,17 +1,11 @@
-const { exec } = require("child_process");
-const util = require("util");
-const execPromise = util.promisify(exec);
+const shell = require("shelljs");
 const path = require("node:path");
-const cwd = process.cwd(); // Current Working Directory
+shell.config.fatal = true;
+shell.config.verbose = true;
 
+const cwd = process.cwd();
 const PACKED_DIR = ".packed";
 const packedDir = path.resolve(cwd, PACKED_DIR);
 
-const publish = async () => {
-  await execPromise(`
-        cd ${packedDir};
-        npm publish;
-  `);
-};
-
-publish();
+shell.cd(packedDir);
+shell.exec("npm publish");
