e62b377532d79876ed21e702b6816b2f2522bd42
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-12-13 15:38:10 +0400

    build script improvements

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,10 @@ shell.config.verbose = true;
 const SOURCE_DIR = "src";
 const BUILD_DIR = "dist";
 
-shell.rm("-rf", "dist");
+shell.rm("-rf", BUILD_DIR);
+shell.cp("-R", SOURCE_DIR, BUILD_DIR);
+shell.rm("-rf", `${BUILD_DIR}/**/*.scss`);
+shell.rm("-rf", `${BUILD_DIR}/**/*.js`);
 shell.exec(
   `npx sass --no-source-map --stop-on-error --color ${SOURCE_DIR}:${BUILD_DIR}`,
   { silent: true }
