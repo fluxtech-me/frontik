2aeca43b5566a1053838728d6ad8d210da9a2222
Gevorg Margaryan <107540875+g-margaryan@users.noreply.github.com>
2022-12-26 15:38:35 +0400

    Create broken-standards.js

---
+++ src/device/broken-standards.js
@@ -0,0 +1,11 @@
+function (a, b, c, d = 8, e) {
+  if ( a ===b ) {
+    if (a > c) {
+      if ( c == b) {
+        return true;
+      }
+    }
+  } else if ( d == c) {
+    return  b + e + 100
+  }
+}
