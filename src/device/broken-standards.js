c89c74ee3d0385bb0b206c57db33406f36dc7e1d
Gevorg Margaryan <107540875+g-margaryan@users.noreply.github.com>
2022-12-26 15:43:55 +0400

    Update broken-standards.js

--- src/device/broken-standards.js
+++ src/device/broken-standards.js
@@ -2,7 +2,7 @@ function (a, b, c, d = 8, e) {
   if ( a ===b ) {
     if (a > c) {
       if ( c == b) {
-        return true;
+        return {a: 12, b: 'meesage'};
       }
     }
   } else if ( d == c) {
